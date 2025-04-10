b0b28ade6b087e6c9f1004ed78ef8e67975b0eb0
KonstBeliakov <konstbeliakov@gmail.com>
2025-04-10 14:45:07 +0300

    little refactoring

--- point.py
+++ point.py
@@ -25,25 +25,28 @@ def get_point_or_random(scene, name: str | None) -> Point:
 
 
 def to_point(scene, point: str | Point | tuple[int | float, int | float] | list[int | float, int | float] | None):
-    if point is None:
-        return Point(scene)
-    if isinstance(point, str):
-        return get_point_or_random(scene, point)
-    if isinstance(point, Point):
-        return point
-    if isinstance(point, tuple) or isinstance(point, list):
-        if len(point) != 2:
-            raise ValueError(f"Can't create a point from {type(point)} of length {len(point)}.")
-        if not (isinstance(point[0], int) or isinstance(point[0], float)):
-            raise ValueError(f"Can't create a point from {point}, because of wrong type of the x coordinate.")
-        if not (isinstance(point[1], int) or isinstance(point[1], float)):
-            raise ValueError(f"Can't create a point from {point}, because of wrong type of the y coordinate.")
+    match point:
+        case None:
+            return Point(scene)
+        case str():
+            return get_point_or_random(scene, point)
+        case Point():
+            return point
+        case tuple() | list():
+            if len(point) != 2:
+                raise ValueError(f"Can't create a point from {type(point)} of length {len(point)}.")
+            if not (isinstance(point[0], int) or isinstance(point[0], float)):
+                raise ValueError(f"Can't create a point from {point}, because of wrong type of the x coordinate.")
+            if not (isinstance(point[1], int) or isinstance(point[1], float)):
+                raise ValueError(f"Can't create a point from {point}, because of wrong type of the y coordinate.")
 
-        for p in point_names.values():
-            if abs(p.x - point[0]) < 10 ** -6 and abs(p.y - point[1]) < 10 ** -6:
-                return p
+            for p in point_names.values():
+                if abs(p.x - point[0]) < 10 ** -6 and abs(p.y - point[1]) < 10 ** -6:
+                    return p
 
-        return Point(scene, x=point[0], y=point[1])
+            return Point(scene, x=point[0], y=point[1])
+        case default:
+            raise TypeError(f'Can\'t create a point from the argument of type {type(point)}')
 
 
 class Point:
